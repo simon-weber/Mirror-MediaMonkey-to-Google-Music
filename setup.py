58a56f2d6c33c91417d0abde863b35123ef5419f
Simon Weber <sweb090@gmail.com>
2012-04-04 18:29:51 -0400

    add preliminary packaging info

---
+++ setup.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2012, Simon Weber
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the copyright holder nor the
+#       names of the contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from distutils.core import setup
+
+setup(
+    name='mm2gm',
+    version='<next>',
+    author='Simon Weber',
+    author_email='simon@simonmweber.com',
+    url='http://pypi.python.org/pypi/gmusicapi/',
+    packages=['sync2gm'],
+    scripts=['mm2gm.py'],
+    license='COPYING',
+    description='Mirrors/syncs a MediaMonkey library to Google Music.',
+    long_description="""\
+
+""",
+    install_requires=[
+        "gmusicapi >= 2012.04.03",
+        "sqlite3"
+    ],
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Sound/Audio",
+        ],
+    include_package_data=True,
+    zip_safe=False,
+)
