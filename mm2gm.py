c6bc3e5fcc56f5d1a09296e69b387e2f1020633b
Simon Weber <sweb090@gmail.com>
2012-04-06 17:50:19 -0400

    start to flesh out handlers

--- mm2gm.py
+++ mm2gm.py
@@ -3,82 +3,179 @@
 
 """Allows syncing of a MediaMonkey database to Google Music."""
 
-from sync2gm import DBChangeHandler, TriggerDef
+from sync2gm import TriggerDef, ConfigPair, HandlerResult, MDMapping, make_md_map, get_gm_id
+from gmusicapi import Api, CallFailure
 
-## config start ##
-triggers = [
-#Song triggers:
-TriggerDef(name='sync2gm_cSong',
-           table='Songs',
-           when="AFTER INSERT",
-           idValText='new.ID'),
+#Define how to set up the connection, since MediaMonkey needs a custom collation function.
+#It won't allow string queries without it.
+#Credit to Sproaticus: http://www.mediamonkey.com/forum/viewtopic.php?p=127635#127635
+def make_connection(db_path):
+    """Return a connection to the database at *db_path*."""
 
-TriggerDef(name='sync2gm_uSong',
-           table='Songs',
-           when="AFTER UPDATE OF (Artist, Album, AlbumArtist, Comment, Genre, Rating, Year, DiscNumber, TrackNumber, BPM, SongTitle)",
-           idValText='new.ID'),
+    def iUnicodeCollate(s1, s2):
+        return cmp(s1.lower(), s2.lower())
 
-TriggerDef(name='sync2gm_dSong',
-           table='Songs',
-           when="AFTER DELETE",
-           idValText='old.ID')
+    conn = sqlite3.connect(db_path)
+    conn.create_collation('IUNICODE', iUnicodeCollate)
+    #There are also USERLOCALE and NUMERICSTRING collations referred to here:
+    #http://www.mediamonkey.com/wiki/index.php/MediaMonkey_Database_structure
 
-#Playlist triggers:
-TriggerDef(name='sync2gm_cPlaylist',
-           table='Playlists',
-           when="AFTER INSERT",
-           idValText='new.IDPlaylist'),
+    return conn
 
-TriggerDef(name='sync2gm_uPlaylistName',
-           table='Playlists',
-           when="AFTER UPDATE OF (PlaylistName)",
-           idValText='new.IDPlaylist'),
+#Define metadata mapping.
+def to_gm_rating(r):
+    if r == -1:
+        return 0
+    if 0 <= r < 50:
+        return 1
+    else:
+        return 5
 
-TriggerDef(name='sync2gm_addPlaylistSong',
-           table='PlaylistSongs',
-           when="AFTER INSERT",
-           idValText='new.IDPlaylistSong'),
+md_mappings = [
+    #make_md_map defaults gm_key to col with a lowercase first char,
+    # and to_gm_form to identity (ie same form).
+    make_md_map('Artist'),
+    make_md_map('Album'),
+    make_md_map('AlbumArtist'),
+    make_md_map('Comment'),
+    make_md_map('Genre'),
+    make_md_map('Rating', to_gm_form = to_gm_rating),
+    make_md_map('Year'),
 
-TriggerDef(name='sync2gm_delPlaylistSong',
-           table='PlaylistSongs',
-           when="AFTER DELETE",
-           idValText='old.IDPlaylistSong'),
+    make_md_map('DiscNumber', 'disc'),
+    make_md_map('TrackNumber', 'track'),
+    make_md_map('BPM', 'beatsPerMinute'),
+    make_md_map('SongTitle', 'name')
+    ]
 
-TriggerDef(name='sync2gm_movePlaylistSong',
-           table='PlaylistSongs',
-           when="AFTER UPDATE OF (SongOrder)",
-           idValText='new.IDPlaylistSong'),
-
-#Autoplaylist-specific triggers:
-TriggerDef(name='sync2gm_uAPlaylistQuery',
-           table='Playlists',
-           when="AFTER UPDATE OF (QueryData)",
-           idValText='new.IDPlaylist')
-]
-
-#List of MM db columns that map directly to GM md keys (once in camelCase).
-# eg MM: "AlbumArtist" -> GM "albumArtist"
-
-cols_same = ("Artist", "Album", "AlbumArtist", "Comment", 
-             "Genre", "Rating", "Year")
-
-#Mapping of MM db cols to their GM md key.
-
-cols_diff = {"DiscNumber": "disc",
-             "TrackNumber": "track",
-             "BPM": "beatsPerMinute",
-             "SongTitle": "name"}
 
 #Not sure what to do with these GM keys yet:
 # totalTracks totalDiscs - can't find the MM db entry
 # albumArtUrl - unsure how GM handles uploading/updating album art
 
+#Map col name to it's MDMapping.
+col_to_mdm = {}
+for mdm in md_mappings:
+    col_to_mdm[mdm.col] = mdm
+
+#Get the mm cols into sql col format; col place holders aren't allowed.
+mm_sql_cols = repr(tuple(col_to_mdm.keys())).replace("'","")[1:-1]
+
+
+#define handlers.
+#These receive three args: localId, api (an already authenticated gmusicapi) and conn (an sqlite3 connection).
+#conn uses sqlite3.Row as conn.row_factory.
+#They are expected to do whatever is needed to push out changes.
+
+#They should use the sync2gm_GMSongIds, sync2gm_GMPlaylistIds, and sync2gm_ProblemIds tables.
+
+#They do not need to check for success, but can raise CallFailure,
+# sqlite.Error or UnmappedId, which the service will handle.
+
+#All handlers that create/delete remote items must return a HandlerResult.
+#This allows the service to keep track of local -> remote mappings.
+
+def cSongHandler(localId, api, conn):
+    conn.execute("SELECT SongPath from Songs WHERE ID=?", (localId,))
+    path = conn.fetchone()
+
+    res = api.upload(path)
+
+    if res.get(path) is None:
+        raise CallFailure
+
+    return HandlerResult(action='create', itemType='song', gmId=res[path])
+
+
+def uSongHandler(localId, api, conn):
+    mm_md = conn.execute("SELECT %s FROM Songs WHERE ID=?" % mm_sql_cols, (localId,)).fetchone()
+
+    gm_song = {}
+    for col in mm_md.keys():
+        mdm = mm_to_mdm[col]
+
+        gm_key = mdm.gm_key
+        data = mdm.translate(mm_md)
+
+        gm_song[gm_key] = data
+
+    gm_song['id'] = get_gms_id(localId)
+
+    api.change_song_metadata(gm_song) #TODO should switch this to a safer method
+    
+def dSongHandler(localId, api, conn):
+    delIds = api.delete_songs(get_gms_id(localId))
+
+    return HandlerResult(action='delete', itemType='song', gmId=delIds[0])
+
+def cPlaylistHandler(localId, api, conn):
+    #currently assuming that this is called prior to any inserts on PlaylistSongs
+    pass
+    
+
+config = [
+    #Song config
+    ConfigPair(
+        trigger = TriggerDef(
+            name='sync2gm_cSong',
+            table='Songs',
+            when="AFTER INSERT",
+            idValText='new.ID'),
+        handler=cSongHandler),
+    
+    ConfigPair(
+        trigger = TriggerDef(
+            name='sync2gm_uSong',
+            table='Songs',
+            when="AFTER UPDATE OF (%s)" % mm_sql_cols,
+            idValText='new.ID'),
+        handler = uSongHandler),
+    
+    ConfigPair(
+        trigger = TriggerDef(
+            name='sync2gm_dSong',
+            table='Songs',
+            when="AFTER DELETE",
+            idValText='old.ID'),
+        handler = dSongHandler),
+
+    #Playlist config
+    ConfigPair(
+        trigger = TriggerDef(
+            name='sync2gm_cPlaylist',
+            table='Playlists',
+            when="AFTER INSERT",
+            idValText='new.IDPlaylist'),
+        handler = cPlaylistHandler),
+
+
+trigger=TriggerDef(name='sync2gm_uPlaylistName',
+           table='Playlists',
+           when="AFTER UPDATE OF (PlaylistName)",
+           idValText='new.IDPlaylist'),
+
+trigger=TriggerDef(name='sync2gm_addPlaylistSong',
+           table='PlaylistSongs',
+           when="AFTER INSERT",
+           idValText='new.IDPlaylistSong'),
+
+trigger=TriggerDef(name='sync2gm_delPlaylistSong',
+           table='PlaylistSongs',
+           when="AFTER DELETE",
+           idValText='old.IDPlaylistSong'),
+
+trigger=TriggerDef(name='sync2gm_movePlaylistSong',
+           table='PlaylistSongs',
+           when="AFTER UPDATE OF (SongOrder)",
+           idValText='new.IDPlaylistSong'),
+
+#Autoplaylist-specific triggers:
+trigger=TriggerDef(name='sync2gm_uAPlaylistQuery',
+           table='Playlists',
+           when="AFTER UPDATE OF (QueryData)",
+           idValText='new.IDPlaylist')
+]
 
 ## config end ##
 
 
-mm_to_gm_cols = {}
-for mm_c in cols_same:
-    mm_to_gm_cols[mm_c] = mm_c[0].lower + mm_c[1:]
-
-mm_to_gm_cols = dict(mm_to_gm_cols.items() + cols_diff.items())
--- sync2gm/sync2gm.py
+++ sync2gm/sync2gm.py
@@ -20,17 +20,63 @@ from twisted.protocols.basic import LineReceiver
 #reactor.listenTCP(8123, ChatFactory())
 #reactor.run()
 
-#A change handler is associated with a change number.
-# push_change is the function that is called to send out its changes
-# args_query is an optional list of sql SELECT queries to run, each of which is given as a positional param to push_change, when called.
-#  the queries are given a named parameter :localId.
-DBChangeHandler = collections.namedtuple('DBChangeHandler', ['push_change', 'args_query'])
-TriggerDef = collections.namedtupled('TriggerDef', ['name', 'table', 'when', 'idValText'])
+
+TriggerDef = collections.namedtuple('TriggerDef', ['name', 'table', 'when', 'idValText'])
 #when surrounded by eg
 # CREATE TRIGGER {name} when ON {table}
 
+#A list of these defines the config: a trigger, and the handler for it.
+ConfigPair = collections.namedtuple('Config', ['trigger', 'handler'])
+
+#Holds the result from a handler, so the service can keep local -> remote mapping up to date.
+#action: one of {'create', 'delete'}. Updates can just return an empty HandlerResult.
+#itemType: one of {'song', 'playlist'}
+#gmId: <string>
+HandlerResult = collections.namedtuple('HandlerResult', ['action', 'itemType', 'gmId'])
+
+#Maps a local column to a piece of gm metadata.
+MDMapping = collections.namedtuple('MDMapping', ['col', 'gm_key', 'to_gm_form'])
+
+def make_md_map(col, gm_key=None, to_gm_form=None):
+    """Create a new MDMapping conviniently."""
+    if gm_key is None:
+        gm_key = col[0].lower() + col[1:]
+
+    if to_gm_form is None:
+        to_gm_form = lambda data: data
+
+    return MDMapping(col, gm_key, to_gm_form)
+
+
 class GMSyncError(Exception):
     pass
+
+class UnmappedId(Exception):
+    """Raised when we expect a mapping exists between local/remote ids,
+    but one does not."""
+    pass
+
+
+def get_gms_id(localId):
+    """Return the GM song id associated with this *localId*, or None."""
+    return _get_gm_id(localId, 'song')
+
+def get_gmp_id(localId):
+    """Return the GM playlist id associated with this *localId*, or None."""
+    return _get_gm_id(localId, 'playlist')
+
+def _get_gm_id(localId, itemType):
+    #Conn needs to come from the service
+    if itemType == 'song':
+        table='sync2gm_GMSongIds'
+    else:
+        table='sync2gm_GMPlaylistIds'
+
+    gmId = conn.execute("SELECT gmId FROM %s WHERE localId=?" % table, (localId,)).fetchone()
+    
+    if not gmId: raise UnmappedId
+    
+    return gmId[0]
 
 #for the following, triggers is a list of TriggerDefs
 def attach(db_filename, triggers):
